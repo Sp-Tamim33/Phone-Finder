c5439d070492c65f45b3484d25d10c5ca4caf692
sp-tam1m <white.hat.tamim@gmail.com>
2022-03-01 21:20:10 +0600

    phone loaded on website

--- js/app.js
+++ js/app.js
@@ -12,7 +12,7 @@ const searchButton = () => {
         .then(data => loadPhone(data.data))
 }
 
-// function for load Phones 
+// function for load Phones in website
 const loadPhone = (data) => {
     // console.log(data);
     const mainDiv = document.getElementById('main-div');
@@ -27,10 +27,61 @@ const loadPhone = (data) => {
             <div class="card-body">
             <h5 class="card-title">Name : ${element.phone_name}</h5>
             <p class="card-title my-3">Brand : ${element.brand}</p>
-                <a href="#" onclick='details(${element.idDrink})' class="btn btn-primary mt-2">Details</a>
+                <a href='#' onclick="details('${element.slug}')" class="btn btn-primary mt-2">Details</a>
             </div>
         </div>`;
         mainDiv.appendChild(div);
     });
 
+}
+
+// function for details button 
+const details = (id) => {
+    const url = `https://openapi.programming-hero.com/api/phone/` + id;
+    // console.log(url);
+    fetch(url)
+        .then(res => res.json())
+        .then(data => loadDetails(data.data))
+}
+
+// function for load details in webiste
+const loadDetails = (detail) => {
+    console.log(detail);
+
+    const detailDiv = document.getElementById('detail-display');
+    detailDiv.textContent = ''
+    // console.log(drink);
+    const div = document.createElement('div');
+    div.innerHTML = `
+        <div class="card mx-auto shadow" style="width: 18rem;">
+        <img class="card-img-top p-4" src="${detail.image}" alt="Card image cap">
+            <div class="card-body">
+            <h5 class="card-title">Name :${detail.name}</h5>
+            <p class="card-title">Brand : <strong>${detail.brand}</strong></p>
+            <li>${detail.mainFeatures.releaseDate}</li>
+                <div><strong>Features :</strong>
+                    <li>${detail.mainFeatures.storage}</li>
+                    <li>${detail.mainFeatures.displaySize}</li>
+                    <li>${detail.mainFeatures.chipSet}</li>
+                    <li>${detail.mainFeatures.memory}</li>
+                </div>
+                <div class="mt-2"><strong>Sensors :</strong>
+                    <li>${detail.mainFeatures.sensors[0]}</li>
+                    <li>${detail.mainFeatures.sensors[1]}</li>
+                    <li>${detail.mainFeatures.sensors[2]}</li>
+                    <li>${detail.mainFeatures.sensors[3]}</li>
+                </div>
+                <div class="mt-2"><strong>Others :</strong>
+                    <li>${detail.others.WALN}</li>
+                    <li>${detail.others.Bluetooth}</li>
+                    <li>${detail.others.GPS}</li>
+                    <li>${detail.others.NFC}</li>
+                    <li>${detail.others.Radio}</li>
+                    <li>${detail.others.USB}</li>
+                </div>
+            </div>
+        </div>
+        `;
+
+    detailDiv.appendChild(div);
 }
